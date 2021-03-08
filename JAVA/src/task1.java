8b0ef25ea6c2ecf4b0b4558638c4747f8f42aced
Muhammad Ahtisham <ahti.mughal7866@gmail.com>
2021-03-08 16:32:21 +0500

    final

---
+++ JAVA/src/task1.java
@@ -0,0 +1,13 @@
+public class task1 {
+
+    String quote="Hello Shampy's World";
+    
+    public static void main(String[] args) {
+        
+        task1 t1 = new task1();
+        
+       System.out.println(t1.quote);
+       
+    }
+    
+}
---
+++ PHP/index.php
@@ -0,0 +1,5 @@
+<?php
+
+Echo "Hello Shampy's World";
+
+?>
